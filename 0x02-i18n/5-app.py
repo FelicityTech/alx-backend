ae385ff4b882cde0a3dbba235c4bbb92fa6a2b08
Solomon Eniola Adegoke <souceking@gmail.com>
2023-03-01 01:57:05 +0000

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
-"""This script starts a Flask web application"""
+
+"""
+5. Basic Flask app
+"""
+
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
+
+
+class Config:
+    """
+    Config class.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -14,56 +30,42 @@ users = {
 }
 
 
-class Config(object):
-    """The Languages Config class"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
-
-
-def get_user():
-    """The get_user function"""
-    user = request.args.get('login_as')
-    if user and int(user) in users.keys():
-        return users.get(int(user))
-    return None
+def get_user(login_as):
+    """
+    get_user.
+    """
+    try:
+        return users.get(int(login_as))
+    except Exception:
+        return
 
 
 @app.before_request
 def before_request():
-    """The before_request function"""
-    user = get_user()
-    if user:
-        g.user = user
+    """
+    before_request
+    """
+    g.user = get_user(request.args.get("login_as"))
 
 
 @babel.localeselector
 def get_locale():
-    """The get_locale function"""
-    lang = request.args.get('locale')
-
-    if lang in app.config['LANGUAGES']:
-        return lang
-
+    """
+    get_locale.
+    """
+    locale = request.args.get("locale")
+    if locale:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-# babel = Babel(app, locale_selector=get_locale)
+@app.route('/', methods=["GET"], strict_slashes=False)
+def hello():
+    """
+    hello.
+    """
+    return render_template('5-index.html')
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """This function executes when the 0.0.0.0:5000/ is requested"""
-    user = getattr(g, 'user', None)
-    if user:
-        username = user.get('name')
-    else:
-        username = None
-    return render_template('5-index.html', username=username)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
